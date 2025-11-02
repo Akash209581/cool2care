45dd98c30d8101cef9c5fb412e0a9178f899702a
Akash209581 <bandaruakash8@gmail.com>
2025-11-02 15:22:57 +0530

    fix: Add polyfills to fix browser compatibility issues

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,7 @@
 // Import polyfills first
 import './polyfills.js'
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
